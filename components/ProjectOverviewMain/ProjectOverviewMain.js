44d67f6166bf1b09c48eb839ce0aaa782674947b
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-03-10 13:46:41 +0530

    using router for nav in projectOverview, fixed shortcuts click bug

--- components/ProjectOverviewMain/ProjectOverviewMain.js
+++ components/ProjectOverviewMain/ProjectOverviewMain.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 
 import styles from "./ProjectOverviewMain.module.scss";
 import DueDate from "../DueDate/DueDate";
@@ -15,39 +14,42 @@ const ProjectOverviewMain = (props) => {
     { description: "Update leveling" },
   ]);
   const router = useRouter();
-  return (
-    <Link
-      href={{
+  const handleOverciewClick = (e) => {
+    if (e.target.nodeName !== "IMG") {
+      console.log("d");
+      router.push({
         pathname: `${router.asPath}/${props.project.projectName.toLowerCase()}`,
         query: { projectId: props.project.projectId },
+        as: `${router.asPath}/${props.project.projectName.toLowerCase()}`,
+      });
+    }
+  };
+  return (
+    <section
+      onClick={handleOverciewClick}
+      className={styles.projectCard}
+      style={{
+        background: `linear-gradient(-45deg, ${props.project.theme[0]}, ${props.project.theme[1]})`,
       }}
-      // as={`${router.asPath}/${props.project.projectName.toLowerCase()}`}
     >
-      <section
-        className={styles.projectCard}
-        style={{
-          background: `linear-gradient(-45deg, ${props.project.theme[0]}, ${props.project.theme[1]})`,
-        }}
-      >
-        <article className={styles.leftBar}>
-          <header className={styles.cardHeader}>
-            <h2>
-              {props.project.projectName}
-              <span className={styles.tag}>#{props.project.tag}</span>
-            </h2>
-            <DueDate days={props.project.dueDate} />
-          </header>
-          <Shortcuts shortcuts={JSON.parse(props.project.shortcuts)} />
-          <ProgressBar storiesDone={20} totalStories={40} />
-        </article>
-        <article className={styles.rightBar}>
-          <header className={styles.rightBarHeader}>
-            <h3>STORIES</h3>
-          </header>
-          <Todos todos={todos} />
-        </article>
-      </section>
-    </Link>
+      <article className={styles.leftBar}>
+        <header className={styles.cardHeader}>
+          <h2>
+            {props.project.projectName}
+            <span className={styles.tag}>#{props.project.tag}</span>
+          </h2>
+          <DueDate days={props.project.dueDate} />
+        </header>
+        <Shortcuts shortcuts={JSON.parse(props.project.shortcuts)} />
+        <ProgressBar storiesDone={20} totalStories={40} />
+      </article>
+      <article className={styles.rightBar}>
+        <header className={styles.rightBarHeader}>
+          <h3>STORIES</h3>
+        </header>
+        <Todos todos={todos} />
+      </article>
+    </section>
   );
 };
 
