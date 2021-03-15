897f060e9715be5f8ea28d374988441386abccc6
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-03-15 13:26:31 +0530

    switched to env for keys

--- auth/firebaseAdmin.js
+++ auth/firebaseAdmin.js
@@ -1,10 +1,20 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../pm-auth.json");
 
 export const verifyIdToken = (token) => {
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        type: "service_account",
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URL,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+        client_x509_cert_url: process.env.CLIENT_CERT_URL,
+      }),
     });
   }
   return admin
